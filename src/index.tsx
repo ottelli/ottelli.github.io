e77137b4af1ac4f4b04d288cdbcad947bc822dac
ottelli <business.ottelli@gmail.com>
2022-12-20 18:01:22 +0000

    remove unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import reportWebVitals from "./reportWebVitals"
-
 import App from "./App"
 // import Dev from "./Development"
 import Error404 from "./components/404"
@@ -20,8 +18,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
